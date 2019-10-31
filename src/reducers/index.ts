aa48a53699aca247c6488bee5e05038411363299
sankari-p <sivasankari.p@accoliteindia.com>
2019-10-31 12:03:36 +0530

    Configured redux store : Included a demo root reducer

---
+++ src/reducers/index.ts
@@ -0,0 +1,11 @@
+const initialState = {
+  loading: true,
+  response: [],
+};
+
+export const rootReducer = (state = initialState, action: any) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
