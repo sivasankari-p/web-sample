aa0d95608398254ef39b3a32b3018917df1e9645
sankari-p <sivasankari.p@accoliteindia.com>
2020-07-28 11:16:05 +0530

    Updates

---
+++ precache-manifest.03f848fe7f19bfeea0a7792bf0a480fc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac5afbbbdc8c66aafcc44a56863e30cf",
+    "url": "/web-sample/index.html"
+  },
+  {
+    "revision": "efaccf1e8d36525eeead",
+    "url": "/web-sample/static/css/main.332656b7.chunk.css"
+  },
+  {
+    "revision": "30b8918f7673437253e0",
+    "url": "/web-sample/static/js/2.0a7d7a1e.chunk.js"
+  },
+  {
+    "revision": "efaccf1e8d36525eeead",
+    "url": "/web-sample/static/js/main.d8fd5ada.chunk.js"
+  },
+  {
+    "revision": "ec21e282dd73efc7cae9",
+    "url": "/web-sample/static/js/runtime-main.a160b299.js"
+  },
+  {
+    "revision": "45c73723862c6fc5eb3d6961db2d71fb",
+    "url": "/web-sample/static/media/fontawesome-webfont.45c73723.eot"
+  },
+  {
+    "revision": "4b5a84aaf1c9485e060c503a0ff8cadb",
+    "url": "/web-sample/static/media/fontawesome-webfont.4b5a84aa.woff2"
+  },
+  {
+    "revision": "76a4f23c6be74fd309e0d0fd2c27a5de",
+    "url": "/web-sample/static/media/fontawesome-webfont.76a4f23c.svg"
+  },
+  {
+    "revision": "7c87870ab40d63cfb8870c1f183f9939",
+    "url": "/web-sample/static/media/fontawesome-webfont.7c87870a.ttf"
+  },
+  {
+    "revision": "dfb02f8f6d0cedc009ee5887cc68f1f3",
+    "url": "/web-sample/static/media/fontawesome-webfont.dfb02f8f.woff"
+  }
+]);
